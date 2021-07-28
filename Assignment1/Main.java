e4cb73f78f570c10de391401efc4ce5d557842ad
Siddharth Singh <sidsgsits@gmail.com>
2021-07-28 20:28:48 +0530

    problem 1, Problem1 class is the class with private methods to access data from a url and using regex to filter out the main data and returning in the form of String, Main class access method getdata() to print the output. Now only sorting is to be done to print top three words from words.txt file.

---
+++ Assignment1/Main.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Problem1 obj = new Problem1();
+		obj.getdata();
+//		HashMap<String, Integer> map = new HashMap<>();
+//		try {
+//			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\hp\\Desktop\\words.txt"));
+//			String s;
+//			
+//			while((s=br.readLine())!=null) {
+//				int cc = Problem1.readingFile(s);
+//				map.put(s, cc);
+//			}
+//			br.close();
+//		} catch (FileNotFoundException e) {
+//			
+//			e.printStackTrace();
+//		}
+//		catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println(map);
+	}
+
+}
---
+++ Assignment1/Problem1.java
@@ -0,0 +1,99 @@
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+   
+
+public class Problem1 {
+	public void PrintOutput(String data) {
+		String[] words = data.split(" ");
+		HashMap<String,Integer> map = new HashMap<>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("words.txt"));
+			String s;
+			
+			while((s=br.readLine())!=null) {
+				int cc = Problem1.readingFile(data,s);
+				map.put(s, cc);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(map);
+	}
+	public static int readingFile(String data,String findw) {
+		int count=0;
+		
+//			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\hp\\Documents\\Java Files\\WebData.txt"));
+//			String Stemp=br.readLine();
+			String[] words = data.split(" ");
+			for(String x:words) {
+				if(x.toLowerCase().contains(findw.toLowerCase())) {
+					count++;
+				}
+			}
+//			br.close();
+			
+		return count;
+	}
+	public void getdata() {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("url.txt"));
+			
+			String s;
+			while((s=br.readLine())!=null) {
+//				BufferedWriter bw = new BufferedWriter(new FileWriter(new File("C:\\Users\\hp\\Documents\\Java Files\\WebData.txt")));
+				String output = Problem1.getUrlContents(s);
+				output = output.replaceAll("<[^>]*>", "").replaceAll("(?m)^\\s+$", "").replaceAll("\\s", " ");
+				PrintOutput(output);
+//				bw.write(output);
+				
+//				bw.close();
+			}
+			
+			br.close();
+			
+			  
+		    
+		    
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	 static String getUrlContents(String theUrl)  
+	  {  
+	    StringBuilder content = new StringBuilder();  
+	  // Use try and catch to avoid the exceptions  
+	    try  
+	    {  
+	      URL url = new URL(theUrl); // creating a url object  
+	      URLConnection urlConnection = url.openConnection(); // creating a urlconnection object  
+	  
+	      // wrapping the urlconnection in a bufferedreader  
+	      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));  
+	      String line;  
+	      // reading from the urlconnection using the bufferedreader  
+	      while ((line = bufferedReader.readLine()) != null)  
+	      {  
+	        content.append(line + "\n");  
+	      }  
+	      bufferedReader.close();  
+	    }  
+	    catch(Exception e)  
+	    { 
+	    	
+	      e.printStackTrace();  
+	    }  
+	    return content.toString();  
+	  }  
+}
