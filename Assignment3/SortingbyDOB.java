17ac82b5251a7f1e4dbed780e7fcc446ef5cb8ec
Siddharth Singh <sidsgsits@gmail.com>
2021-08-01 16:46:02 +0530

    Implemented the Sortingbyname, Sortingbyage and SortingbyDOB using Comparator.

---
+++ Assignment3/SortingbyDOB.java
@@ -0,0 +1,25 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;  
+
+public class SortingbyDOB implements Comparator<Employee>{
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		try {
+			Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(o1.getDOB());
+			Date date2=new SimpleDateFormat("dd/MM/yyyy").parse(o2.getDOB());
+			return date1.compareTo(date2);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+		
+		
+	}
+	
+
+}
---
+++ Assignment3/Sortingbyname.java
@@ -0,0 +1,13 @@
+
+import java.util.Comparator;
+
+public class Sortingbyname implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		
+		return o1.getName().compareTo(o2.getName());
+	}
+	
+
+}
