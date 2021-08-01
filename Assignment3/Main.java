9f1fdbebba50a8862b392ec619579f2f93b1b10d
Siddharth Singh <sidsgsits@gmail.com>
2021-08-01 15:40:31 +0530

    Made all the required classes for the program only Implementation is remaining, Written some code of Problem3 class: ConsoleMenu which prints the console and takes the input from user about his choice; Addtofile method adds particular Employee given as argumnet by the method AddOp which takes the data of new Employee. hashmap is also used to save the employee data and whenever the program starts its execution it is initialised with the Employee.txt file such that it has Previous data of Employee so that it can used when User wants to Search in th data. SearchOp and DeleteOp is unimplemented.

---
+++ Assignment3/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		Problem3 pr = new Problem3();
+		pr.InitializeHash();
+//		pr.PrintallEmp();
+
+		while(true) {
+			pr.ConsoleMenu();
+		}
+		
+		
+	}
+
+}
---
+++ Assignment3/Problem3.java
@@ -0,0 +1,126 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Problem3 {
+	
+	private HashMap<String,Employee> empList = new HashMap<>();
+	private Scanner sc = new Scanner(System.in);
+	public void InitializeHash() {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("Employee.txt"));
+			String values = br.readLine();
+			String[] all = values.split(";");
+			for(String x : all) {
+				String[] data = x.split(",");
+				Employee emp = new Employee(data[1], data[2], Integer.parseInt(data[3]), data[4]);
+				empList.put(getEmpID(emp), emp);
+			}
+			br.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+		
+	}
+	public void ConsoleMenu() {
+		
+		System.out.println("\n\tMenu");
+		System.out.println("1) Add Employee ");
+		System.out.println("2) Delete Employee ");
+		System.out.println("3) Search Employee ");
+//		System.out.println("4) Print Employees ");
+		System.out.print("Your Choice Number-> ");
+		byte choice = sc.nextByte();
+		sc.nextLine();
+		Operations(choice);
+	}
+	public void Operations(byte b) {
+		
+		switch (b) {
+		case 1:
+			AddOp();
+			break;
+		case 2:
+			DeleteOp();
+			break;
+		case 3:
+			SearchOp();
+			break;
+//		case 4:
+//			PrintallEmp();
+//			break;
+		default:
+			System.out.println("Ivalid Choice!!");
+			break;
+		}
+	}
+	private void SearchOp() {
+		
+		
+	}
+	private void DeleteOp() {
+		
+		
+	}
+	public void PrintallEmp() {
+		
+		for (Entry<String, Employee> entry : empList.entrySet()) {
+			Employee e = entry.getValue();
+			System.out.println(e.getName()+", "+e.geteMail()+", "+e.getAge()+", "+e.getDOB());
+		}
+		
+	}
+	private String getEmpID(Employee e) {
+		String s = e.getName();
+		s=s.substring(0, 3);
+		String date = e.getDOB();
+		String[] d = date.split("/");
+		return (s+d[0]+d[1]+d[2]);
+	}
+	private void AddOp() {
+		
+		System.out.println("Adding Employee!!");
+		System.out.print("Enter Name: ");
+		String name = sc.nextLine();
+		
+		System.out.print("Enter Email: ");
+		String mail = sc.nextLine();
+		System.out.print("Enter Age: ");
+		int age = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Enter DOB: ");
+		String dob = sc.nextLine();
+		
+		Employee emp = new Employee(name, mail, age, dob);
+		empList.put(getEmpID(emp), emp);
+		Addtofile(emp);
+		System.out.println("Employee Added!!");
+	}
+	public void Addtofile(Employee e) {
+		
+		try {
+			
+			BufferedWriter br = new BufferedWriter(new FileWriter("Employee.txt",true));
+			if(new File("Employee.txt").length()==0) {
+				br.write(getEmpID(e)+",");
+			}
+			else {
+				br.write(";"+getEmpID(e)+",");
+			}
+					
+			br.write(""+e.getName()+",");
+			br.write(""+e.geteMail()+",");
+			br.write(""+e.getAge()+",");
+			br.write(""+e.getDOB());
+			br.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+	}
+	
+}
