22ad3f03d3d27327f3672d620123b31b9bebba63
Siddharth Singh <sidsgsits@gmail.com>
2021-08-02 12:37:40 +0530

    Added the Two Main Class SerializationTest and DeserializationTest, Now making output1.ser and output2.ser files with current Student class.

---
+++ Assignment5/DeserializationTest.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+public class DeserializationTest {
+
+	public static void main(String[] args) {
+		Problem5 pr5 = new Problem5();
+		String filename="output1.ser";
+		ArrayList<Student> data = pr5.DeserializingList(filename);
+		for(Student s: data) {
+			System.out.print(s.firstName+" "+s.dob);
+			System.out.println(" "+s.a.city+" "+s.a.State+" "+s.a.pinCode+" "+s.a.country);
+			
+		}
+
+	}
+
+}
---
+++ Assignment5/SerializationTest.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class SerializationTest {
+
+	public static void main(String[] args) {
+		Problem5 pr5 = new Problem5();
+		String filename="output2.ser";
+		pr5.SerializationProcess(filename);
+		
+		
+		
+	}
+
+}
