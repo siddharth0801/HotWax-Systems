5b2856a01d564ca99a160256b0b93c056282c44f
Siddharth Singh <sidsgsits@gmail.com>
2021-08-13 15:51:18 +0530

    made student class encapsulated.

--- Assignment5/Student.java
+++ Assignment5/Student.java
@@ -6,21 +6,39 @@ import java.util.Date;
 
 public class Student implements Serializable {
 
-	String firstName;
-	Date dob;
-	Address a;
+	private String firstName;
+	private Date dob;
+	private Address a;
 	public Student(String name,String dob,Address a) {
-		this.firstName=name;
+		this.setFirstName(name);
 		setDate(dob);
-		this.a=a;
+		this.setA(a);
 	}
 	private void setDate(String dob) {
 		try {
 			Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(dob);
-			this.dob=date1;
+			this.setDob(date1);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public Address getA() {
+		return a;
+	}
+	public void setA(Address a) {
+		this.a = a;
 	}
 	
 }
